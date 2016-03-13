ea83dbb773d8e946ad98423e5aa3761f89b5d3be
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-03-13 09:27:51 +0100

    react to sad people

--- static/app/app.js
+++ static/app/app.js
@@ -1,35 +1,60 @@
 /**
  * Created by casassg on 12/03/16.
  */
-$(function() {
+$(function () {
     var intro_section = $('.intro');
     var loading_section = $('.loading');
     var happy_section = $('.happy');
     var noface_section = $('.noface');
+    var sad_section = $('.sad');
     var session_id = null;
     var old_result = 0;
-    $('.start').on('click', function() {
+    $('.start').on('click', function () {
         intro_section.hide();
         happy_section.hide();
         noface_section.hide();
+        sad_section.hide();
         loading_section.show();
         $.ajax({
-            url:'picture/take',
+            url: 'picture/take',
             dataType: "json"
         }).done(function (data) {
             loading_section.hide();
-            session_id= data.id;
-            if (data.result=='NOFACE'){
+            session_id = data.id;
+            if (data.result == 'NOFACE') {
                 noface_section.show();
             }
-            else{
-                show_result(old_result,data.result)
+            else {
+                show_result(old_result, data.result)
             }
         })
     });
 
-    function show_result(old_r,new_r) {
-        var dif =new_r-old_r;
+    function show_result(old_r, new_r) {
+        intro_section.hide();
+        happy_section.hide();
+        noface_section.hide();
+        sad_section.hide();
+        loading_section.hide();
+        if (new_r == 'NOFACE') {
+            noface_section.show();
+            return;
+        }
+        var dif = new_r - old_r;
+        if (dif > 0) {
+            happy_section.show()
+        } else {
+            old_result = new_r;
+            sad_section.show();
+            setTimeout(function () {
+                $.ajax({
+                    url: 'picture/react/' + session_id,
+                    dataType: "json"
+                }).done(function (data) {
+                    show_result(old_result, data.result)
+                });
+            }, 3000);
 
+        }
     }
 });
